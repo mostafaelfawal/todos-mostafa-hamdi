a27992269397075229d0a5be19d398bc12c61408
mostafaelfawal <armostafa982@gmail.com>
2025-12-01 04:02:17 +0200

    fix server error

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,17 @@ app.use(cors({
 app.use(express.json());
 app.use("/api/tasks", router);
 
-mongoose
-  .connect(MONGO_URI)
-  .then(() => console.log("✅ MongoDB Connected"))
-  .catch((err) => console.log("Error: " + err));
+let isConnected = false;
+async function connectDB() {
+  if (isConnected) return;
+  await mongoose.connect(process.env.MONGO_URI);
+  isConnected = true;
+  console.log("MongoDB Connected");
+}
+
+app.use(async (_, _, next) => {
+  await connectDB();
+  next();
+});
 
 app.listen(PORT, () => console.log(`🚀server running on port ${PORT}`));
