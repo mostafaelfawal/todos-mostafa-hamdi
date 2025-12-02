c260a8bffca599d340156eed9358fca2faf04c52
mostafaelfawal <armostafa982@gmail.com>
2025-12-02 20:11:13 +0200

    fix backend request

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,7 +1,13 @@
-import app from '../app.js';
-import connectDB from '../config/db.js';
+import app from "../app.js";
+import connectDB from "../config/db.js";
+import serverless from "serverless-http";
+
+let cachedHandler;
 
 export default async function handler(req, res) {
-  await connectDB();
-  return app(req, res);
+  if (!cachedHandler) {
+    await connectDB();
+    cachedHandler = serverless(app);
+  }
+  return cachedHandler(req, res);
 }
